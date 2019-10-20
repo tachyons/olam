8a84f9bbf21ecb2bb71ae04421397497cb1c869a
Aboobacker MK <aboobackervyd@gmail.com>
2019-10-20 12:31:50 +0530

    Introduce olam database wrapper (#22)
    * Introduce Olam database wrapper

---
+++ src/olamdatabase.h
@@ -0,0 +1,18 @@
+#ifndef OLAMDATABASE_H
+#define OLAMDATABASE_H
+
+#include <QSqlDatabase>
+
+
+
+class OlamDatabase
+{
+private:
+    QSqlDatabase db;
+    QString detect_language(QString word);
+public:
+    OlamDatabase();
+    QStringList suggestions(QString word);
+};
+
+#endif // OLAMDATABASE_H
